c8c1fbf32e319e2448d82c7cd73c7045b5a05645
Kovalev Pavel <kovalev5690@gmail.com>
2023-04-26 14:16:05 +0300

    Remove thread limit

--- src/main.cpp
+++ src/main.cpp
@@ -7,8 +7,6 @@ int main() {
   std::size_t proc_count = std::thread::hardware_concurrency();
   if (proc_count > 16) {
     proc_count = 16;
-  } else {
-    proc_count /= 2;
   }
 
   for (const auto& [x, y] : GetData(0, tN, proc_count)) {
