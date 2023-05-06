f69b2192c6e08ff3b8337ea02038b7b514b7da79
Kovalev Pavel <kovalev5690@gmail.com>
2023-05-06 19:05:26 +0300

    Follow c++20 not c++2b

--- src/matrix.cpp
+++ src/matrix.cpp
@@ -6,13 +6,13 @@ Matrix::Matrix(const std::size_t& height, const std::size_t& width) noexcept
   data_.reserve(height * width);
 }
 
-Matrix::value_type& Matrix::operator[](const std::size_t& idx_x,
-                                       const std::size_t& idx_y) noexcept {
+Matrix::value_type Matrix::Get(const std::size_t& idx_x,
+                               const std::size_t& idx_y) const noexcept {
   return data_[idx_y * width_ + idx_x];
 }
 
-Matrix::value_type const& Matrix::operator[](
-    const std::size_t& idx_x, const std::size_t& idx_y) const noexcept {
+Matrix::value_type& Matrix::get_ref(const std::size_t& idx_x,
+                                    const std::size_t& idx_y) noexcept {
   return data_[idx_y * width_ + idx_x];
 }
 
@@ -23,11 +23,13 @@ Matrix Matrix::Cholecky() const noexcept {
     for (std::size_t j = 0; j <= i; ++j) {
       value_type sum = 0;
       for (std::size_t k = 0; k < j; ++k)
-        sum += l[i, k] * l[j, k];
+        sum += l.Get(i, k) * l.Get(j, k);
+
+      auto& cell = l.get_ref(i, j);
       if (i == j)
-        l[i, j] = std::sqrt((*this)[i, i] - sum);
+        cell = std::sqrt(this->Get(i, i) - sum);
       else
-        l[i, j] = (1.0 / l[j, j] * ((*this)[i, j] - sum));
+        cell = (1.0 / l.Get(j, j) * (this->Get(i, j) - sum));
     }
   }
 
--- src/matrix.h
+++ src/matrix.h
@@ -10,15 +10,15 @@ class Matrix {
   explicit Matrix(const std::size_t& height, const std::size_t& width) noexcept;
   ~Matrix() noexcept = default;
 
-  value_type& operator[](const std::size_t& idx_x,
-                         const std::size_t& idx_y) noexcept;
-
-  value_type const& operator[](const std::size_t& idx_x,
+  [[nodiscard]] value_type Get(const std::size_t& idx_x,
                                const std::size_t& idx_y) const noexcept;
 
   [[nodiscard]] Matrix Cholecky() const noexcept;
 
  private:
+  value_type& get_ref(const std::size_t& idx_x,
+                      const std::size_t& idx_y) noexcept;
+
   std::size_t height_;
   std::size_t width_;
 
